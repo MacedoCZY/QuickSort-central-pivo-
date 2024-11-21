3512080709542e004bb81c4f8479d1cfcd18aaa8
Macedo <86130318+MacedoCZY@users.noreply.github.com>
2024-11-21 13:49:02 -0300

    readfile

--- QuickSort.c
+++ QuickSort.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-void main(){
+int main(int argc, char *argv[]){
+	//printf("%d\n", argc);
+	if(argc == 2){
+		FILE *file = fopen(argv[1], "r");
+		if(file == NULL){
+			printf("Erro open archive!\n");
+			return -1;
+		}
+		
+		int buffer;
+		while(feof(file) == 0){
+			fread(&buffer, sizeof(int), 1, file);
+			printf("%d\n", buffer);
+		}
 
+		fclose(file);
+	}else{
+		printf("Erro in arguments!\n");
+		return -1;
+	}
 }
