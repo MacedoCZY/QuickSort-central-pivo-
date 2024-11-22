710226f3371db58d566fc24761a410a97d466e1d
Macedo <86130318+MacedoCZY@users.noreply.github.com>
2024-11-22 17:00:36 -0300

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]){
-	//printf("%d\n", argc);
 	if(argc == 2){
-		FILE *file = fopen(argv[1], "r");
+		FILE *file = fopen(argv[1], "rb");
+		
 		if(file == NULL){
 			printf("Erro open archive!\n");
 			return -1;
 		}
-		
-		int buffer;
-		while(feof(file) == 0){
-			fread(&buffer, sizeof(int), 1, file);
-			printf("%d\n", buffer);
-		}
+
+		fseek(file, 0, SEEK_END);
+
+    	int size = ftell(file);
+		char buffer[__SCHAR_MAX__];
+		int work[size];
+
+		fseek(file, 0, SEEK_SET);
+
+		printf("Size %d\n", size);
+
+		int i = 0;
+		while(fread(buffer, sizeof(char), sizeof(buffer), file) != 0) {
+        	work[i] = atoi(buffer);
+			printf("%s", buffer);
+			i++;
+    	}
+
 
 		fclose(file);
 	}else{
 		printf("Erro in arguments!\n");
 		return -1;
 	}
+	return 0;
 }
