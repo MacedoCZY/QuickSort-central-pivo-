6add743ae00bf31c593aa0fcaff7553909016e04
Macedo <86130318+MacedoCZY@users.noreply.github.com>
2024-11-23 11:29:57 -0300

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -18,25 +18,25 @@ int main(int argc, char *argv[]){
 
 		fseek(file, 0, SEEK_END);
 
-    	int size = ftell(file);
+    	int size = (ftell(file)/2)+0.5;
 		int buffer[size];
-		printf("size : %d", size);
+		printf("size : %d\n", size);
 		fseek(file, 0, SEEK_SET);
 
 		int i = 0;
 		int x = 0;
-		//fscanf (file, "%d", &x);   
-		// while (!feof (file))
-		// {  
-		// 	fscanf (file, "%d", &x);
-		// 	buffer[i] = x;
-		// 	i++;      
-		// }
+		fscanf (file, "%d", &x);   
+		while (!feof (file))
+		{  
+			buffer[i] = x;
+			fscanf (file, "%d", &x);
+			i++;      
+		}
 
 		for(int i = 0; i < size; i++){
 			printf("%d\n", buffer[i]);
 		}
-		//quickSort(buffer, 0, size);
+		quickSort(buffer, 0, sizeof(buffer));
 
 		fclose(file);
 	}else{
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]){
 
 void quickSort(int values[], int left, int right){
 	if (left < right) {
-		printf("entro\n");
+		printf("quickSort\n");
 		int pivot = partition(values, left, right);
 		quickSort(values, left, pivot - 1);
 		quickSort(values, pivot + 1, right);	
@@ -56,12 +56,14 @@ void quickSort(int values[], int left, int right){
 }
 
 void swap(int* a, int* b){
+	printf("swap\n");
     int aux = *a;
     *a = *b;
     *b = aux;
 }
 
 int partition(int arr[], int left, int right){
+	printf("partition\n");
     int mid = left + (right - left) / 2;
     int pivot = arr[mid];
     
